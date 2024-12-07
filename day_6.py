3dd665f0483429b881803749c028084c00de8464
james.marvin <james.marvin@naimuri.com>
2024-12-07 12:13:09 +0000

    Getting there

--- day_6.py
+++ day_6.py
@@ -41,44 +41,31 @@ def get_path(start_point: Point, direction: Point, grid: Grid) -> list:
     return path
         
 
-def traverse_from(
-    start_point: Point, 
-    direction: Point, 
-    grid: Grid, 
-    level: int=0
-) -> None:
-
+def traverse_from(start_point: Point, direction: Point, grid: Grid) -> None:
+    """Follow a series of paths until we leave the grid."""
     current_point = start_point
     path=[]
-    loop_closure_points = []
 
     while True:
-    
-        # The next point we want to go to
-        next_point = current_point + direction
-        next_direction = get_next_direction(direction)
+        next_leg = get_path(current_point, direction, grid)
+        path.extend(next_leg[:-1])
 
-        # If that point would exit the map
-        if not next_point.is_valid(grid):
+        if not next_leg[-1].end_point.is_valid(grid):
             break
-
-        # If that point is an obstacle
-        if grid.at(next_point) in obstructions:
+        
+        if grid.at(next_leg[-1].end_point) in obstructions:
+            current_point = next_leg[-1].start_point
             direction = get_next_direction(direction)
-            continue
-
-        # If we're observed this move before
-        if Line(current_point, next_point) in path:
-            raise RecursionError("This traversal appears to loop infinitely")
-            break
-
-        path.append(Line(current_point, next_point))
-        current_point = next_point
 
     return path
 
+output = traverse_from(Point(4, 6), Point(0, -1), my_grid)
 
-test = get_path(Point(5, 6), Point(0, -1), my_grid)
+points = []
+for line in output:
+    for point in line.points:
+        if point not in points:
+            points.append(point)
 
-for line in test:
-    print(line, my_grid.at(line.end_point))
+print(len(points))
+
